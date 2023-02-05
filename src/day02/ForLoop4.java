9acd5d621b535c85119e5984497986172cd74bb3
Elifisci1103@gmail.com <you@example.com>
2023-02-05 14:24:13 +0300

    v4

---
+++ src/day02/ForLoop4.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class ForLoop4 { public static void main(String[] args) {
+    // input olarak verilen bir Stringde
+    // index'i tek sayi olanlari kucuk harfle
+    // index'i cift sayi olanlari buyuk harfle yazdirin
+    // ornek : Java  output: JaVa
+    String input= "Java candir, Selenium heyecandir";
+    for (int i = 0; i <input.length() ; i++) {
+        System.out.print  ( i%2==0   // index cift mi ?
+                ? input.substring(i,i+1).toUpperCase() // index cift ise
+                : input.substring(i,i+1).toLowerCase() // index cift degilse
+        );
+    }
+}
+}

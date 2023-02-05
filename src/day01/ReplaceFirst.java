9ff59643bf1fca8715595d755e7d4e97aec1937b
Elifisci1103@gmail.com <you@example.com>
2023-02-05 14:14:26 +0300

    v2

---
+++ src/day01/ReplaceFirst.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class ReplaceFirst {
+    public static void main(String[] args) {
+        String str= "Java heyecandir";
+        // eger tum a'lari degil de sadece ilk a'yi degistirmek isterseniz
+        System.out.println(str.replaceFirst("a", "A"));// JAva heyecandir
+        // ilk harf veya rakami * yap
+        System.out.println(str.replaceFirst("\\w", "*"));
+    }
+}

99f3405740ae8e340a5f81a3f4d36d631be173f5
Elifisci1103@gmail.com <you@example.com>
2023-02-05 14:22:09 +0300

    v3

---
+++ src/day02/ForLoop3.java
@@ -0,0 +1,15 @@
+package day02;
+
+import java.util.Scanner;
+
+public class ForLoop3 {  public static void main(String[] args) {
+    // kullanicidan bir char alip
+    // o karakterden sonra gelen 10 karakteri yazdirin
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Lutfen bir karakter giriniz");
+    char girilenKarakter=scan.next().charAt(0);
+    for (int i = 1; i <=10 ; i++) {
+        System.out.print((char) (girilenKarakter+i) + " ");
+    }
+}
+}
